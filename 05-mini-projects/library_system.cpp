e783bd613f7182a94b22541b14e1cdcdec248f2b
Hassan5737 <hasanhazem2016@gmail.com>
2026-04-11 23:31:05 +0200

    update library_system.cpp file

--- 05-mini-projects/library_system.cpp
+++ 05-mini-projects/library_system.cpp
@@ -23,28 +23,170 @@ public:
         price = p;
     }
 
-    bool operator==(const book& other)
+    bool operator==(const book& other) const
     {
-        return (this->title == other.title && this->author == other.author);
+        return (title == other.title && author == other.author);
     }
 
-    book operator+(const book& other)
+    book operator+(const book& other) const
     {
         book result;
 
-        result.price = this->price + other.price;
-        result.title = this->title + " & " + other.title;
-        result.author = this->author + " & " + other.author;
+        result.price = price + other.price;
+        result.title = title + " & " + other.title;
+        result.author = author + " & " + other.author;
 
         return result;
     }
 
+    double getPrice() const
+    {
+        return price;
+    }
+
     void print() const
     {
-        cout << "Book title: " << title << endl;
-        cout << "Book author: " << author << endl;
-        cout << "Book price: " << price << endl;
+        cout << "Title: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "Price: " << price << endl;
     }
 
     ~book() {}
 };
+
+class library
+{
+private:
+    book* books;
+    int size;
+
+public:
+    // Default constructor
+    library()
+    {
+        books = nullptr;
+        size = 0;
+    }
+
+    // Parameterized constructor (Deep Copy)
+    library(book arr[], int s)
+    {
+        size = s;
+        books = new book[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            books[i] = arr[i];
+        }
+    }
+
+    // Copy Constructor (Deep Copy)
+    library(const library& other)
+    {
+        size = other.size;
+        books = new book[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            books[i] = other.books[i];
+        }
+    }
+
+    // Destructor
+    ~library()
+    {
+        delete[] books;
+    }
+
+    // Print all books
+    void print() const
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << "Book " << i + 1 << ":\n";
+            books[i].print();
+            cout << "--------\n";
+        }
+    }
+
+    // operator[]
+    book& operator[](int index)
+    {
+        return books[index];
+    }
+
+    // operator+
+    library operator+(const library& other) const
+    {
+        library result;
+
+        result.size = size + other.size;
+        result.books = new book[result.size];
+
+        // copy first library
+        for (int i = 0; i < size; i++)
+        {
+            result.books[i] = books[i];
+        }
+
+        // copy second library
+        for (int i = 0; i < other.size; i++)
+        {
+            result.books[i + size] = other.books[i];
+        }
+
+        return result;
+    }
+
+    // operator==
+    bool operator==(const library& other) const
+    {
+        if (size != other.size)
+            return false;
+
+        for (int i = 0; i < size; i++)
+        {
+            if (!(books[i] == other.books[i]))
+                return false;
+        }
+
+        return true;
+    }
+
+    // total price
+    double totalPrice() const
+    {
+        double sum = 0;
+
+        for (int i = 0; i < size; i++)
+        {
+            sum += books[i].getPrice();
+        }
+
+        return sum;
+    }
+};
+
+int main()
+{
+    book b1("A", "X", 100);
+    book b2("B", "Y", 200);
+    book b3("C", "Z", 300);
+
+    book arr1[] = {b1, b2};
+    book arr2[] = {b3};
+
+    library l1(arr1, 2);
+    library l2(arr2, 1);
+
+    library l3 = l1 + l2;
+
+    l3.print();
+
+    cout << "First book:\n";
+    l3[0].print();
+
+    cout << "Total price: " << l3.totalPrice() << endl;
+
+    return 0;
+}
