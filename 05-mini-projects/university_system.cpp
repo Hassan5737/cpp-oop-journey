ebf0cbff0fe638fa6dfb456c9614818afa980604
Hassan5737 <hasanhazem2016@gmail.com>
2026-04-18 19:49:50 +0200

    update 3 to university_system

--- 05-mini-projects/university_system.cpp
+++ 05-mini-projects/university_system.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 
-
-class person
-{
+// ======================
+// Abstract Base Class
+// ======================
+class Person {
 protected:
     string name;
     int id;
 
 public:
-    person(string n = "unknown", int i = 0)
+    Person(string n = "unknown", int i = 0)
     {
         name = n;
         id = i;
@@ -23,80 +24,230 @@ public:
 
     virtual double calculateSalary() const = 0;
 
-    virtual ~person() {}
+    virtual ~Person() {}
 };
 
-
-class student : public person
-{
-protected:
+// ======================
+// Student Class
+// ======================
+class Student : public Person {
+private:
     double* grades;
     int size;
 
 public:
-    student() : person()
+    Student() : Person()
     {
         grades = nullptr;
         size = 0;
     }
 
-    student(string n, int i, double arr[], int z) : person(n, i)
+    Student(string n, int i, double arr[], int z) : Person(n, i)
     {
         size = z;
         grades = new double[size];
 
         for (int i = 0; i < size; i++)
-        {
             grades[i] = arr[i];
-        }
     }
 
-student(const student &other) : person(other)
-{
-    size = other.size;
-
-    grades = new double[size];
-
-    for (int i = 0; i < size; i++)
+    // Copy Constructor (Deep Copy)
+    Student(const Student& other) : Person(other)
     {
-        grades[i] = other.grades[i];
-    }
-}
+        size = other.size;
+        grades = new double[size];
 
-    // Destructor
-    ~student()
+        for (int i = 0; i < size; i++)
+            grades[i] = other.grades[i];
+    }
+
+    double averageGrade() const
+    {
+        if (size == 0) return 0;
+
+        double sum = 0;
+        for (int i = 0; i < size; i++)
+            sum += grades[i];
+
+        return sum / size;
+    }
+
+    void print() const override
+    {
+        Person::print();
+
+        cout << "Grades: ";
+        for (int i = 0; i < size; i++)
+            cout << grades[i] << " ";
+
+        cout << "\nAverage: " << averageGrade() << endl;
+    }
+
+    double calculateSalary() const override
+    {
+        return 0;
+    }
+
+    ~Student()
     {
         delete[] grades;
     }
-
 };
 
+// ======================
+// Teacher Class
+// ======================
+class Teacher : public Person {
+private:
+    double salary;
+    double bonus;
 
-int main 
+public:
+    Teacher(string n, int i, double s, double b) : Person(n, i)
+    {
+        salary = s;
+        bonus = b;
+    }
+
+    void print() const override
+    {
+        Person::print();
+        cout << "Salary: " << salary << endl;
+        cout << "Bonus: " << bonus << endl;
+    }
+
+    double calculateSalary() const override
+    {
+        return salary + bonus;
+    }
+};
+
+// ======================
+// University Class
+// ======================
+class University {
+private:
+    Person** people;
+    int size;
+
+public:
+    // Default Constructor
+    University()
+    {
+        people = nullptr;
+        size = 0;
+    }
+
+    // Parameterized Constructor
+    University(Person* arr[], int s)
+    {
+        size = s;
+        people = new Person*[size];
+
+        for (int i = 0; i < size; i++)
+            people[i] = arr[i]; // shallow copy (intentional)
+    }
+
+    // Destructor
+    ~University()
+    {
+        delete[] people;
+    }
+
+    // Print all
+    void printAll() const
+    {
+        for (int i = 0; i < size; i++)
+        {
+            people[i]->print();
+            cout << "------------\n";
+        }
+    }
+
+    // Total Salaries
+    double totalSalaries() const
+    {
+        double sum = 0;
+
+        for (int i = 0; i < size; i++)
+            sum += people[i]->calculateSalary();
+
+        return sum;
+    }
+
+
+    University operator+(const University& other) const
+    {
+        University result;
+
+        result.size = size + other.size;
+        result.people = new Person*[result.size];
+
+        for (int i = 0; i < size; i++)
+            result.people[i] = people[i];
+
+        for (int i = 0; i < other.size; i++)
+            result.people[i + size] = other.people[i];
+
+        return result;
+    }
+
+   
+    Person* operator[](int index)
+    {
+        return people[index];
+    }
+
+    // Operator ==
+    bool operator==(const University& other) const
+    {
+        if (size != other.size)
+            return false;
+
+        for (int i = 0; i < size; i++)
+        {
+            if (people[i] != other.people[i])
+                return false;
+        }
+
+        return true;
+    }
+};
+
+// ======================
+// MAIN
+// ======================
+int main()
 {
-    Student s1("Hassan", 1, grades1, 3);
-Student s2("Ali", 2, grades2, 3);
+    double g1[] = {80, 90, 85};
+    double g2[] = {70, 60, 75};
 
-Teacher t1("Dr.A", 10, 5000, 500);
-Teacher t2("Dr.B", 11, 6000, 600);
+    Student s1("Hassan", 1, g1, 3);
+    Student s2("Ali", 2, g2, 3);
 
-Person* arr1[] = { &s1, &t1 };
-Person* arr2[] = { &s2, &t2 };
+    Teacher t1("Dr.A", 10, 5000, 500);
+    Teacher t2("Dr.B", 11, 6000, 600);
 
-University u1(arr1, 2);
-University u2(arr2, 2);
+    Person* arr1[] = { &s1, &t1 };
+    Person* arr2[] = { &s2, &t2 };
 
-University u3 = u1 + u2;
+    University u1(arr1, 2);
+    University u2(arr2, 2);
 
-u3.printAll();
+    University u3 = u1 + u2;
 
-cout << "Total Salaries: " << u3.totalSalaries() << endl;
+    cout << "=== All People ===\n";
+    u3.printAll();
 
-u3[0]->print();
+    cout << "Total Salaries: " << u3.totalSalaries() << endl;
 
-if (u1 == u2)
-    cout << "Same\n";
-else
-    cout << "Different\n";
+    cout << "\nFirst Person:\n";
+    u3[0]->print();
+
+    if (u1 == u2)
+        cout << "\nSame\n";
+    else
+        cout << "\nDifferent\n";
+
     return 0;
 }
