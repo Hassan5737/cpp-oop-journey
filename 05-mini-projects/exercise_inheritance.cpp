3f99c270ad8543af50228da906f823a4dcaad212
Hassan Ahmed Hassan <hasanhazem2016@gmail.com>
2026-05-10 22:05:18 +0300

    add 1 in exercise_inheritance

--- 05-mini-projects/exercise_inheritance.cpp
+++ 05-mini-projects/exercise_inheritance.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
 
+// base class
+class Father
+{
+    Father()
+    {
+        cout << " hi i'm first constroctor" << endl:
+    }
+}
+
 int main()
 {
     return 0;
