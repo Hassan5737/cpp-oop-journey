f88fc62c104b0b7954dca6e5b77ee45783f3a245
Hassan Ahmed Hassan <hasanhazem2016@gmail.com>
2026-05-14 19:48:50 +0300

    add 14 in exercise_inheritance

--- 05-mini-projects/exercise_inheritance.cpp
+++ 05-mini-projects/exercise_inheritance.cpp
@@ -25,3 +25,41 @@ Types of Inheritance:
 
 =========================================================
 */
+
+// =====================================================
+// 1) SINGLE INHERITANCE
+// =====================================================
+
+// Parent Class
+class Animal
+{
+protected:
+    string name;
+
+public:
+
+    Animal(string n)
+    {
+        name = n;
+    }
+
+    void eat()
+    {
+        cout << name << " is eating" << endl;
+    }
+};
+
+// Child Class
+class Dog : public Animal
+{
+public:
+
+    Dog(string n) : Animal(n)
+    {
+    }
+
+    void bark()
+    {
+        cout << name << " is barking" << endl;
+    }
+};
