9cef1791af07ad84cd02b384de6d85fb201d8027
Hassan Ahmed Hassan <hasanhazem2016@gmail.com>
2026-05-16 23:09:48 +0300

    add first exercise

--- 05-mini-projects/exercise_oop.cpp
+++ 05-mini-projects/exercise_oop.cpp
@@ -1,3 +1,53 @@
 #include <iostream>
 using namespace std;
 
+/*
+====================================================
+                OOP Exercise in C++
+====================================================
+
+Requirements:
+
+Create a class called Student.
+
+The class should contain:
+
+Private:
+- name
+- id
+- gpa
+
+Public:
+- Default Constructor
+- Parameterized Constructor
+- Setter Functions
+- Getter Functions
+- displayInfo() function
+- isExcellent() function
+
+====================================================
+Tasks in main():
+====================================================
+
+1. Create an object using the Default Constructor.
+2. Set the values using Setter Functions.
+3. Print the student information.
+4. Check if the student is excellent.
+5. Create another object using the Parameterized Constructor.
+6. Print the second student information.
+
+====================================================
+Expected OOP Concepts:
+====================================================
+
+- Classes & Objects
+- Encapsulation
+- Constructors
+- Default Constructor
+- Parameterized Constructor
+- Setter & Getter
+- Member Functions
+- Boolean Functions
+
+====================================================
+*/
