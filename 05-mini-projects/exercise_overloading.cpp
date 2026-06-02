45d95abd1a816d252be6c19ababe99089e260912
Hassan Ahmed Hassan <hasanhazem2016@gmail.com>
2026-06-02 17:12:51 +0300

    add in exercise_overloading

--- 05-mini-projects/exercise_overloading.cpp
+++ 05-mini-projects/exercise_overloading.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class Dontworry
+{
+    private:
+        int x = 0;
+    public:
+        void setval()
+        {
+            cout << "enter the value: " << endl;
+            cin >> x;
+        }
+};
+
 int main()
 {
     return 0;
