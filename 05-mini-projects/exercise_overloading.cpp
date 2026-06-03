a1ac0af8316504701b3cc44da5656561d397273b
Hassan Ahmed Hassan <hasanhazem2016@gmail.com>
2026-06-03 14:10:43 +0300

    add exercise no.1

--- 05-mini-projects/exercise_overloading.cpp
+++ 05-mini-projects/exercise_overloading.cpp
@@ -1,18 +1,49 @@
 #include <iostream>
 using namespace std;
 
-class Dontworry
-{
-    private:
-        int x = 0;
-    public:
-        void setval()
-        {
-            cout << "enter the value: " << endl;
-            cin >> x;
-        }
-};
+/*
+====================================================
+            first    OOP Exercise
+====================================================
 
+Write a complete program to represent
+a class called Sum.
+
+====================================================
+Requirements:
+====================================================
+
+1. Create a class called Sum.
+
+2. Define one data member:
+   - x
+
+3. Create two objects:
+   - ob1
+   - ob2
+
+4. Add the two objects using:
+   ob1 + ob2
+
+5. Implement the solution using
+   Operator Overloading.
+
+6. Solve the problem using
+   three different methods.
+
+====================================================
+Expected OOP Concepts:
+====================================================
+
+- Classes & Objects
+- Constructors
+- Operator Overloading
+- Member Function
+- Friend Function
+- Returning Objects
+
+====================================================
+*/
 int main()
 {
     return 0;
