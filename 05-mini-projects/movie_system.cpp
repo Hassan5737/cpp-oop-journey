3a4ca552a05aeb41ec4794178443dcf2bb0c5730
Hassan Ahmed Hassan <hasanhazem2016@gmail.com>
2026-06-08 19:45:31 +0300

    add the requirements for Movie system

---
+++ 05-mini-projects/movie_system.cpp
@@ -0,0 +1,280 @@
+/*
+==========================================================
+                Movie Management System
+==========================================================
+
+Goal:
+Build a complete OOP project that reviews all major
+concepts learned so far.
+
+==========================================================
+1) Abstract Base Class
+==========================================================
+
+Create an abstract class:
+
+Media
+
+Protected:
+---------
+string title;
+int year;
+
+Public:
+------
+- Default Constructor
+- Parameterized Constructor
+- Virtual print() function
+- Pure Virtual function:
+
+    virtual double getRating() const = 0;
+
+- Virtual Destructor
+
+==========================================================
+2) Derived Class: Movie
+==========================================================
+
+Inherits publicly from Media.
+
+Private:
+--------
+string director;
+double rating;
+
+Public:
+-------
+- Default Constructor
+- Parameterized Constructor
+- Override print()
+- Override getRating()
+
+==========================================================
+3) Derived Class: Series
+==========================================================
+
+Inherits publicly from Media.
+
+Private:
+--------
+int seasons;
+double rating;
+
+Public:
+-------
+- Default Constructor
+- Parameterized Constructor
+- Override print()
+- Override getRating()
+
+==========================================================
+4) Review Class
+==========================================================
+
+Represents user reviews.
+
+Private:
+--------
+double* scores;
+int size;
+
+Public:
+-------
+- Default Constructor
+- Parameterized Constructor
+
+- Deep Copy Constructor
+
+- Destructor
+
+- double averageScore() const
+
+- print()
+
+==========================================================
+5) Operator Overloading
+==========================================================
+
+Inside Review:
+
+A) operator+
+
+Combine two reviews into one.
+
+Example:
+
+Review r3 = r1 + r2;
+
+----------------------------------------------------------
+
+B) operator[]
+
+Access a score by index.
+
+Example:
+
+cout << r1[0];
+
+----------------------------------------------------------
+
+C) operator==
+
+Compare two Review objects.
+
+==========================================================
+6) Friend Function
+==========================================================
+
+Create friend function:
+
+compareReviews()
+
+Returns the Review with higher average.
+
+==========================================================
+7) Friend Class
+==========================================================
+
+Create class:
+
+ReviewAnalyzer
+
+Make it friend of Review.
+
+Can access private scores directly.
+
+Function:
+
+showHighestScore()
+
+==========================================================
+8) Library Class
+==========================================================
+
+MovieLibrary
+
+Private:
+--------
+Media** items;
+int size;
+
+Public:
+-------
+- Default Constructor
+- Parameterized Constructor
+
+- Destructor
+
+- printAll()
+
+- averageRating()
+
+==========================================================
+9) Polymorphism
+==========================================================
+
+Store:
+
+Movie objects
+Series objects
+
+Inside:
+
+Media* array
+
+Example:
+
+Media* p;
+
+p = &movie;
+p = &series;
+
+==========================================================
+10) Operator Overloading For Library
+==========================================================
+
+A) operator+
+
+Merge two libraries.
+
+Example:
+
+MovieLibrary lib3 = lib1 + lib2;
+
+----------------------------------------------------------
+
+B) operator[]
+
+Access item by index.
+
+Example:
+
+lib[0]->print();
+
+----------------------------------------------------------
+
+C) operator==
+
+Compare two libraries.
+
+==========================================================
+11) Static Member
+==========================================================
+
+Inside Media:
+
+static int mediaCount;
+
+Tracks total number of created objects.
+
+Create:
+
+static void showCount();
+
+==========================================================
+12) Const Functions
+==========================================================
+
+Use const whenever appropriate.
+
+Examples:
+
+print() const
+getRating() const
+averageScore() const
+
+==========================================================
+13) Main Function
+==========================================================
+
+Create:
+
+- Movies
+- Series
+- Reviews
+
+Test:
+
+- Deep Copy
+- Polymorphism
+- Friend Function
+- Friend Class
+- Operator+
+- Operator[]
+- Operator==
+- Static Member
+
+Print final results clearly.
+
+==========================================================
+Bonus Challenges (Optional)
+==========================================================
+
+[ ] Input operator >>
+[ ] Output operator <<
+[ ] Unary Operator
+[ ] Enumeration Genre
+[ ] Copy Assignment Operator
+
+==========================================================
+*/
